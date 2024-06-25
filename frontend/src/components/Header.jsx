29b7573cdaf8668e7840b6d9154e7c5b81aeec01
Sunil Brijlall <brijsunil2@gmail.com>
2024-06-25 14:12:05 -0400

    Implement functionality for blackTextCheckbox, now changes text on page to black on check

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,14 +3,19 @@ import "../styles/Header.css";
 
 const Header = () => {
   const onCheckBoxChange = (e) => {
-    // console.log("Checkbox state [Header]: " + e.target.checked)
+    console.log("Checkbox state [Header]: " + e.target.checked)
+    if (e.target.checked) {
+      document.body.classList.add('text-black');
+    } else {
+      document.body.classList.remove('text-black');
+    }
   }
 
   return (
     <header className="container py-3">
-      <Container className="px-3 text-end">
-        <input type="checkbox" className="" id="black-text-checkbox" onChange={(e) => onCheckBoxChange(e)}></input>
-        <label className="form-check-label mx-2" htmlFor="black-text-checkbox">Make Font Color Black</label>
+      <Container className="px-3">
+        <input type="checkbox" className="" id="blackTextCheckbox" onChange={(e) => onCheckBoxChange(e)}></input>
+        <label className="form-check-label mx-2" htmlFor="blackTextCheckbox">Black Font</label>
       </Container>
     </header>
   );
