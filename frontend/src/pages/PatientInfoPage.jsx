99e385d35ec1e75e13fb897ce8a38f3005a5c7ad
Sunil Brijlall <brijsunil2@gmail.com>
2024-06-25 13:52:18 -0400

    Completed format and styling for PatientInfoPage

--- frontend/src/pages/PatientInfoPage.jsx
+++ frontend/src/pages/PatientInfoPage.jsx
@@ -1,9 +1,10 @@
 import "../styles/PatientInfoPage.css";
 import logo from "../assets/triage_care_logo.png";
-import { Container } from "react-bootstrap";
+import { Container, Button } from "react-bootstrap";
 import HealthCardForm from "../components/HealthCardForm";
 import PatientInfoForm from "../components/PatientInfoForm";
 import PatientContactInfoForm from "../components/PatientContactInfoForm";
+import { IoIosArrowForward } from "react-icons/io";
 
 const PatientInfoPage = () => {
   return (
@@ -27,6 +28,17 @@ const PatientInfoPage = () => {
 
         <PatientInfoForm />
         <PatientContactInfoForm />
+
+        <div className="d-flex flex-row-reverse">
+          <Button
+            className="px-4 py-2 my-4 d-flex align-items-center"
+            style={{verticalAlign: "bottom"}}
+            onClick={() => console.log("Submit patient info [PatientInfoPage]")}
+          >
+            Next
+            <IoIosArrowForward />
+          </Button>
+        </div>
       </Container>
     </main>
   );
